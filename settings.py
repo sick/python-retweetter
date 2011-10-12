624e4371f06d568fbe8eb7a2c367d287fed31530
Ilya Lindberg <ilya.lindberg@gmail.com>
2011-10-12 17:17:27 +0400

    Settings file added

---
+++ settings.py
@@ -0,0 +1,14 @@
+import os.path
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+
+debug = True
+username = 'cgevent'
+hashtag = '#cgevent'
+lastid = os.path.join(PROJECT_PATH, 'lastid.txt')
+ignore_list = os.path.join(PROJECT_PATH, 'ignore_list.txt')
+filtered_word_list = os.path.join(PROJECT_PATH, 'filtered_word_list.txt')
+consumer_key = ''
+consumer_secret = ''
+key = ''
+secret = ''
